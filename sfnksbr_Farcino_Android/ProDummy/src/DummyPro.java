c6a28ec3c4c930c843ec7c623b99bc96708110d2
RobinMartschini <s2310237014@fh-ooe.at>
2024-04-25 14:58:55 +0200

    second commit

--- sfnksbr_Farcino_Android/ProDummy/src/DummyPro.java
+++ sfnksbr_Farcino_Android/ProDummy/src/DummyPro.java
@@ -2,8 +2,14 @@
 public class DummyPro {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+
+		int a = 2;
+		int b = 5;
+		
+		if (a<b) System.out.print("Hello");
+		else System.out.print("Ola");
+		
 	}
 
 }
