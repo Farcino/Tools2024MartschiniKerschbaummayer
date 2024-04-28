f8638bbaf219c71acc77cce20a2ed769f5bb898d
Stefan Kerschbaummayer <ralixo.tv@gmail.com>
2024-04-28 18:44:33 +0200

    Commit im Bugfix Brunch

--- sfnksbr_Farcino_Android/ProDummy/src/DummyPro.java
+++ sfnksbr_Farcino_Android/ProDummy/src/DummyPro.java
@@ -2,23 +2,12 @@
 public class DummyPro {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		
-		
 		int a = 2;
 		int b = 5;
-		
-		if (a<b) System.out.print("Hallo");
-		else System.out.print("Ola");
-=======
->>>>>>> c6a28ec3c4c930c843ec7c623b99bc96708110d2
-
-
-		int a = 2;
-		int b = 5;
-		
+		int c = a + b;
 		if (a<b) System.out.print("Hello");
 		else System.out.print("Ola");
+		if (c) System.out.print("Brua");
 		
 	}
 
