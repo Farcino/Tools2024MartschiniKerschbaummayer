23eae7d7810e7c4c20c6d9284b2f4ac76b740788
RobinMartschini <s2310237014@fh-ooe.at>
2024-04-28 19:01:33 +0200

    first Bugfix

--- sfnksbr_Farcino_Android/ProDummy/src/DummyPro.java
+++ sfnksbr_Farcino_Android/ProDummy/src/DummyPro.java
@@ -1,13 +1,24 @@
+import java.io.IOException;
 
 public class DummyPro {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		
+		int nummer = System.in.read();
 		int a = 2;
 		int b = 5;
 		int c = a + b;
-		if (a<b) System.out.print("Hello");
-		else System.out.print("Ola");
-		if (c) System.out.print("Brua");
+		String geht = "Sollte klappen!";
+		
+		
+		
+		
+		
+		if (nummer < a) {System.out.print("Hello! ");}
+		else {System.out.print("Ola! ");}
+		if (nummer == c) {System.out.print("Brua! ");}
+		
+		System.out.print(geht);
 		
 	}
 
